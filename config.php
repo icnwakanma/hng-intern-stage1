90a1ddd77dbc0bda0d3a065d9f55780b2f98de25
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-08-30 22:23:17 +0100

    added config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+  'database' => [
+      'connection' => 'mysql:host=127.0.0.1',
+      'username' => 'root',
+      'password' => '',
+      'options' => [
+        PDO::ATTR_ERRMODE = PDO::ERRMODE_EXCEPTION
+        ]
+];
